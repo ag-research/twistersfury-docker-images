1709349593678fe25a5f5fd4eeba561c73eab062
Phoenix Osiris <blogger@twistersfury.com>
2018-01-05 21:51:16 -0500

    Adding Nginx/LetsEncrypt Setup

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST';
+
+envsubst "$ENV_VARS" \
+    < /mnt/letsencrypt.template \
+    > /etc/nginx/conf.d/letsencrypt.conf
+
+envsubst "$ENV_VARS" \
+    < /mnt/upstream.template \
+    > /etc/nginx/conf.d/upstream.conf
+
+nginx -t
+
+exec "$@"
