cbd4003d41d763986238cc4005d284619ea4c622
Phoenix Osiris <blogger@twistersfury.com>
2018-01-06 21:40:35 -0500

    Debugging

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,7 +2,13 @@
 
 ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST';
 
-mkdir -p "/etc/letsencrypt/live/$ENV_DOMAIN_NAME";
+if [ -z "/mnt/letsencrypt/live/$ENV_DOMAIN_NAME" ]; then
+    mkdir -p "/etc/letsencrypt/live/$ENV_DOMAIN_NAME";
+
+    mv /mnt/certificate.crt /etc/letsencrypt/live/${ENV_DOMAIN_NAME}/fullchain.pem;
+    mv /mnt/privateKey.key /etc/letsencrypt/live/${ENV_DOMAIN_NAME}/privkey.pem;
+    mv /mnt/certificate.crt /etc/letsencrypt/live/${ENV_DOMAIN_NAME}/chain.pem;
+fi
 
 envsubst "$ENV_VARS" \
     < /mnt/letsencrypt.template \
