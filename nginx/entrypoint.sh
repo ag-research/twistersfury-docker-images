ec0ebce4d8505a55097b10033ced11032deba82c
Phoenix Osiris <blogger@twistersfury.com>
2018-01-06 21:02:35 -0500

    Debugging

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,6 +2,8 @@
 
 ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST';
 
+mkdir -p "/etc/letsencrypt/live/$ENV_DOMAIN_NAME";
+
 envsubst "$ENV_VARS" \
     < /mnt/letsencrypt.template \
     > /etc/nginx/conf.d/letsencrypt.conf
