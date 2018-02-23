33f584c4651550f091eca1a3c3b0de8289dafd8f
Phoenix Osiris <blogger@twistersfury.com>
2018-02-23 12:08:31 -0500

    Adding SSMTP To Cron For Relaying E-Mail

--- cron/entrypoint.sh
+++ cron/entrypoint.sh
@@ -4,6 +4,12 @@ if [ ! -f /mnt/cron.conf ]; then
     echo "* * * * * echo 'Running'" > /mnt/cron.conf
 fi
 
+ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST:$ENV_SMTP_HOST';
+
+envsubst "$ENV_VARS" \
+    < /mnt/ssmtp.template \
+    > /etc/ssmtp/ssmtp.conf
+
 chmod 600 /mnt/cron.conf
 crontab /mnt/cron.conf
 
