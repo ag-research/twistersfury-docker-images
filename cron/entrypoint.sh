f8cbdba80a5b37dd8ef2dbf900d109d24154184b
Phoenix Osiris <phoenix@twistersfury.com>
2018-02-26 22:44:39 +0000

    Moving Cron To Sub Folder

--- cron/entrypoint.sh
+++ cron/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -f /mnt/cron.conf ]; then
-    echo "* * * * * echo 'Running'" > /mnt/cron.conf
+    echo "* * * * * echo 'Running'" > /mnt/cron.d/cron.conf
 fi
 
 ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST:$ENV_SMTP_HOST';
@@ -10,7 +10,7 @@ envsubst "$ENV_VARS" \
     < /mnt/ssmtp.template \
     > /etc/ssmtp/ssmtp.conf
 
-chmod 600 /mnt/cron.conf
-crontab /mnt/cron.conf
+chmod 600 /mnt/cron.d/cron.conf
+crontab /mnt/cron.d/cron.conf
 
 exec "$@"
