200709016da53857f07505a1387c06c0bd3d4f80
Fenikkusu <phoenix@twistersfury.com>
2018-06-20 08:19:44 -0400

    Fix To No Https Mode

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,12 +2,12 @@
 
 ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST';
 
-envsubst "$ENV_VARS" \
-    < /mnt/letsencrypt-http.template \
-    > /etc/nginx/conf.d/letsencrypt-http.conf
-
 # Allow Ability To Disable HTTPS Files (Only Load HTTP)
 if [ -z "$ENV_IGNORE_HTTPS" ]; then
+    envsubst "$ENV_VARS" \
+        < /mnt/letsencrypt-http.template \
+        > /etc/nginx/conf.d/letsencrypt-http.conf
+
     envsubst "$ENV_VARS" \
         < /mnt/letsencrypt-https.template \
         > /etc/nginx/conf.d/letsencrypt-https.conf
