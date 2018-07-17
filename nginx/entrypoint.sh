5ce7169b2a029f2cbe415490c46501384284a5ee
Fenikkusu <phoenix@twistersfury.com>
2018-07-16 23:37:43 -0400

    Tweak To Entry Point

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,12 +2,15 @@
 
 ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST:$ENV_APPLICATION_ENV';
 
-# Allow Ability To Disable HTTPS Files (Only Load HTTP)
-if [ -z "$ENV_IGNORE_HTTPS" ]; then
+# Updating Lets Encrypt
+if [ ! -z "ENV_LETS_ENCRYPT" ]; then
     envsubst "$ENV_VARS" \
         < /mnt/letsencrypt-http.template \
         > /etc/nginx/conf.d/letsencrypt-http.conf
+fi
 
+# Allow Ability To Disable HTTPS Files (Only Load HTTP)
+if [ -z "$ENV_IGNORE_HTTPS" ]; then
     envsubst "$ENV_VARS" \
         < /mnt/letsencrypt-https.template \
         > /etc/nginx/conf.d/letsencrypt-https.conf
