f5a8f52ddc8546c43a87e8a162547a73b45ac3fc
Phoenix Osiris <phoenix@twistersfury.com>
2019-09-21 02:22:10 +0000

    Update entrypoint.sh

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -4,16 +4,19 @@ ENV_VARS='$ENV_DOMAIN_NAME:$ENV_UPSTREAM_HOST:$ENV_APPLICATION_ENV';
 
 # Setup For Lets Encrypt
 if [ -n "ENV_LETS_ENCRYPT" ]; then
+    echo "Running In Lets Encrypt Mode"
     envsubst "$ENV_VARS" \
         < /mnt/letsencrypt-renew.template \
         > /etc/nginx/conf.d/letsencrypt-renew.conf
 # Allow Ability To Disable HTTPS Files (Only Load HTTP)
 elif [ -z "$ENV_IGNORE_HTTPS" ]; then
+    echo "Running In HTTPS Mode"
     envsubst "$ENV_VARS" \
         < /mnt/letsencrypt-https.template \
         > /etc/nginx/conf.d/letsencrypt-https.conf
 # HTTP Only
 else
+    echo "Running In Http Mode"
     envsubst "$ENV_VARS" \
         < /mnt/letsencrypt-http.template \
         > /etc/nginx/conf.d/letsencrypt-http.conf
